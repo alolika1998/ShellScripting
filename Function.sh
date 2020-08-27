3129ecb8fda11998818833ed59540b6a48718886
alolika1998 <66350376+alolika1998@users.noreply.github.com>
2020-08-27 13:28:51 +0530

    Function

---
+++ Function.sh
@@ -0,0 +1,9 @@
+alolika1998@DESKTOP-8QRSH68:~$ touch function.sh
+alolika1998@DESKTOP-8QRSH68:~$ cat > function.sh
+Hello () {
+echo "Hello World $1 $2"
+}
+
+Hello Alolika Mukherjee
+alolika1998@DESKTOP-8QRSH68:~$ bash function.sh
+Hello World Alolika Mukherjee
