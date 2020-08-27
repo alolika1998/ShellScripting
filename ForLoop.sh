1360aa1566afc298d1c53974472139b964483bc0
alolika1998 <66350376+alolika1998@users.noreply.github.com>
2020-08-27 12:55:24 +0530

    For Loop

---
+++ ForLoop.sh
@@ -0,0 +1,18 @@
+alolika1998@DESKTOP-8QRSH68:~$ touch forloop.sh
+alolika1998@DESKTOP-8QRSH68:~$ cat > forloop.sh
+for i in {0..10}
+do
+echo "Start loop .. $i"
+done
+alolika1998@DESKTOP-8QRSH68:~$ bash forloop.sh
+Start loop .. 0
+Start loop .. 1
+Start loop .. 2
+Start loop .. 3
+Start loop .. 4
+Start loop .. 5
+Start loop .. 6
+Start loop .. 7
+Start loop .. 8
+Start loop .. 9
+Start loop .. 10
